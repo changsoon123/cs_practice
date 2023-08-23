6f760a4a88c83e48024bf956e61beef3523f8395
changsoon123 <sool0458@naver.com>
2023-08-24 00:23:52 +0900

    first commit

--- java-singleton/5.java
+++
@@ -1,8 +0,0 @@
-class Singleton {
-    private static class singleInstanceHolder {
-        private static final Singleton INSTANCE = new Singleton();
-    }
-    public static Singleton getInstance() {
-        return singleInstanceHolder.INSTANCE;
-    }
-}
